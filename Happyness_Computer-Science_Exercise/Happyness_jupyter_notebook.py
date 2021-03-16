a9a7e666ae9d36d349a6df8a4214b6284ff30417
alessandro-maccario <alessandro.maccario@gmail.com>
2021-03-16 09:45:54 +0100

    loading datasets

--- Happyness_Computer-Science_Exercise/Happyness_jupyter_notebook.py
+++ Happyness_Computer-Science_Exercise/Happyness_jupyter_notebook.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+
+
+### Question 1
+###
+### Read all input files and put the data in a single dataframe with an additional field year.
+### Notice that the column names are not the same in all input files.
+
+db_2015 = pd.read_csv("2015.csv", sep=";")
+db_2016 = pd.read_csv("2016.csv", sep=";")
+db_2017 = pd.read_csv("2017.csv", sep=";")
+
+print(db_2015)
